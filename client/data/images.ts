a10fdabe589ce366d91bd661b0da5085560c8c02
Lari Ruecker <122192082+popoxta@users.noreply.github.com>
2023-08-18 10:47:33 +1200

    Add image data

---
+++ client/data/images.ts
@@ -0,0 +1,22 @@
+const images = [
+  'https://tldv-wordpress.s3.us-east-2.amazonaws.com/media/20211112090049/Oval-Office-Zoom-background-for-zoom-meetings-the-white-house-min.png',
+  'https://tldv-wordpress.s3.us-east-2.amazonaws.com/media/20211112094557/Tiger-King-Netflix-Documentary-Background-for-Zoom-Funny.png',
+  'https://www.dictionary.com/e/wp-content/uploads/2021/09/20210922_atw_memeStonk_800x800.png',
+  'https://www.researchgate.net/publication/346613754/figure/fig4/AS:964838188675074@1607046758769/Figura-7-Meme-Roll-Safe-Think-About-it-Fuente-Facebook.png',
+  'https://media.tenor.com/-JIgHIMq1u8AAAAC/cool.gif',
+  'https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F034%2F772%2FUntitled-1.jpg',
+ 'https://i.guim.co.uk/img/media/3aab8a0699616ac94346c05f667b40844e46322f/0_123_5616_3432/master/5616.jpg?width=700&quality=85&auto=format&fit=max&s=a476da702aff265ce6f586be1412b1e1',
+ 'https://i.redd.it/2962auyidkyz.jpg',
+  'https://i.redd.it/a1c7m7a0fti41.jpg',
+  'https://i.redd.it/dta0bvkb4t111.jpg',
+  'https://preview.redd.it/vmza0xc2fn631.jpg?width=640&crop=smart&auto=webp&s=f0f9d2205b69f155509fbaa8d43473bca775513c',
+  'https://preview.redd.it/9vqjpisrimm11.jpg?width=640&crop=smart&auto=webp&s=82ec2ee56f27b33637f198fe93c80aa1431a0cf4',
+  'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1551849711-b12648f59767?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1472746729193-36ad213ac4a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80',
+  'https://i.guim.co.uk/img/media/2ff686d2a7b1c1df1226f0c24ff7150e4ac12f61/0_10_4914_2948/master/4914.jpg?width=1200&quality=85&auto=format&fit=max&s=ab206d78062ac78e6b5f2e658eb7b46d',
+  'https://www.shutterstock.com/image-photo/dietbeautiful-young-woman-eating-vegetable-600w-98631656.jpg'
+]
+
+export default images
