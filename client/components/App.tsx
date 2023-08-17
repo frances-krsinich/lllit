412f6d295caaa6370b54b915af696657ad4b878f
popoxta <popoxta@gmail.com>
2023-08-17 17:32:24 +1200

    Add initial state for App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
+const LIVE_LAUGH_LOVE = 'Live, Laugh, Love'
+
 function App() {
+  const [randomWords, setRandomWords] = useState(['these', 'are', 'some', 'boilerplate', 'strings', 'REPLACE_ME!', 'l8r'])
+  const [liveLoveLaugh, setLiveLaughLove] = useState(LIVE_LAUGH_LOVE)
+
   return (
-    <div>
+    <main>
       <h1>App</h1>
       <p>React development has begun!</p>
-    </div>
+    </main>
   )
 }
 
