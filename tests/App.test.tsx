8d420a81ba6cc48bb1f873a4655bce9f7438b54f
Caleb Rapson <136202364+caleb-rapson@users.noreply.github.com>
2023-08-18 14:25:12 +1200

    Add tests for App

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,26 @@
 // @vitest-environment jsdom
 import { describe, it, expect, beforeEach } from 'vitest'
+import { screen, render } from '@testing-library/react'
+import App from '../client/components/App.tsx'
+import images from '../client/data/images.ts'
 
 describe('Test', () => {
-  it('Should run')
+  beforeEach(() => {
+    render(<App />)
+  })
+
+  it('Should render a heading', () => {
+    expect(screen.getByRole('heading', { level: 1 }))
+      .toHaveTextContent('Live it, Laugh it, Love it')
+  })
+
+  it('The quote should initially be Live Laugh Love', () => {
+    expect(screen.getByRole('heading', {level:3}))
+      .toHaveTextContent('Live, Laugh, Love')
+  })
+
+  it('Should initially render a salad image', () => {
+    expect(screen.getByRole('img').src).toBe(images[17])
+  })
+
 })
